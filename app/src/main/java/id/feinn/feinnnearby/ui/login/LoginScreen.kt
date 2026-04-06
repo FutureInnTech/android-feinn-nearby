ab124c70f4382b10fd807b277abbedc542198183
Aziz Anwar <azisanw19@gmail.com>
2026-04-06 20:19:54 +0700

    login screen horizontal orientation can be scrolled

--- app/src/main/java/id/feinn/feinnnearby/ui/login/LoginScreen.kt
+++ app/src/main/java/id/feinn/feinnnearby/ui/login/LoginScreen.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -52,10 +54,11 @@ fun LoginScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(horizontal = 24.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.weight(1.5f))
+            Spacer(modifier = Modifier.height(64.dp))
 
             AppLogo(modifier = Modifier.padding(bottom = 48.dp))
 
@@ -167,7 +170,7 @@ fun LoginScreen(
                 }
             }
 
-            Spacer(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.height(48.dp))
 
             Box(
                 modifier = Modifier
